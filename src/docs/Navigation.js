11c3b46d541c2ddb52a39a0a7a5d9e9c51509429
SamSteele01 <ssteele017@gmail.com>
2017-12-22 15:30:37 -0500

    Change text to variable

--- src/docs/Navigation.js
+++ src/docs/Navigation.js
@@ -8,7 +8,7 @@ const Navigation = ({components}) => {
         components.map( name => {
           return (
             <li key={name}>
-              <a href={`#${name}`}>name</a>
+              <a href={`#${name}`}>{name}</a>
             </li>
           )
         })
