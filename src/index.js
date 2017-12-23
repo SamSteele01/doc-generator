8a375acea7bc36f5485e61f4d486fed67ba28c4e
SamSteele01 <ssteele017@gmail.com>
2017-12-23 18:21:39 -0500

    Add syntax highlighting for code examples

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Docs from './docs/Docs';
+import '../node_modules/highlight.js/styles/atom-one-dark.css'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
